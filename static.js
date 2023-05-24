0aa1455449f91bc4d92b703dbfa388c798da7b44
wikiwoo <velostersnewss@gmail.com>
2023-05-24 21:58:19 +0100

    style

--- static.js
+++ static.js
@@ -1,37 +1,24 @@
-import fs, { createReadStream } from "node:fs";
+import { stat } from "node:fs/promises";
 import path from "node:path";
 import Header from "./Header.js";
-export default function loadStaticFile(res, url) {
-  const parameters = url.split("/");
-  const paths = [];
-  const  urls = [];
-  for (const key in parameters) {
-    if (parameters[key] === null || parameters[key] === "") continue;
-    else paths.push(parameters[key]);
-  }
-
-  for (var t = 0; t < paths.length; t++) {
-    // join path if inlucde assets 
-    if (paths[t].includes("assets")) {
-      if (fs.existsSync(path.resolve(process.cwd() + `/${paths[t]}`))) {
-        urls.push(`/${paths[t]}/${paths[1]}/${paths[2]}`);
+import { createReadStream } from "node:fs";
+export default async function loadStaticFile(res, url) {
+  if ("/" != url) {
+    if (url.includes("assets")) {
+      if (stat(path.resolve(process.cwd() + `${url}`))) {
+        const getContent = createReadStream(
+          path.resolve(process.cwd() + `${url}`)
+        );
+        getContent.on("data", (chunked) => {
+          res.writeHead(200, {
+            "Content-Type": url.includes(".js")
+              ? Header.ContentType.javascipt
+              : Header.ContentType.css,
+          });
+          res.write(chunked);
+          res.end();
+        });
       }
     }
-  }
-  try {
-    for (const key in urls) {
-      const streams = createReadStream(process.cwd() + urls[key]);
-      streams.on("data", (chucked) => {
-        res.writeHead(200, {
-          "Content-Type": urls[key].includes(".js")
-            ? Header.ContentType.javascipt
-            : Header.ContentType.css,
-        });
-        res.write(chucked);
-        res.end();
-      });
-    }
-  } catch (error) {
-    throw new Error(error.message);
   }
 }
